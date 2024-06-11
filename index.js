3f75b025ce6747dbafc4183f879791300b76ec38
khadija <khadijajelle3@gmail.com>
2024-06-11 16:14:41 +0300

    complete functions lab

---
+++ index.js
@@ -0,0 +1,38 @@
+function shout(string) {
+    // todo
+    return string.toUpperCase();
+  }
+  function whisper(string){
+    return string.toLowerCase();
+  }
+  function logwhisper(string){
+    console.log(string.toLowerCase());
+  }
+  function logShout(string){
+    console.log(string.toUpperCase());
+  }
+  logShout("hello");
+  function logwhisper(string){
+    console.log(string.toLowerCase());
+  }
+  
+function logWhisper(string) {
+    console.log(string.toLowerCase());
+}
+  logwhisper("HELLO");
+
+  function sayHiToHeadphonedRoommate(string){
+    if (string === string.toLowerCase()){
+        return "I can't hear you!";
+    }
+    else if (string === string.toUpperCase()){
+        return "YES INDEED!";
+    }
+    else{
+        return "I would love to!";
+    }
+    console.log(string.toLowerCase());
+    
+  } 
+ 
+
